9b105789e0134d93d049fb6634668402f9230a7f
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-06-26 22:15:27 +0300

    feat: integrate next-auth with Google provider for authentication

---
+++ app/_lib/auth.ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+
+const authConfig = {
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
