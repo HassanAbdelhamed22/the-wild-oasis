6c9795419e01e9d93ce8ed924aa80663031c4506
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-06-28 19:15:24 +0300

    feat: implement Google authentication with NextAuth and protected account route

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,19 @@
 import { Metadata } from "next";
+import { auth } from "../_lib/auth";
 
 export const metadata: Metadata = {
   title: "Guest area",
 };
 
-function AccountPage() {
+async function AccountPage() {
+  const session = await auth();
+  const firstName = session?.user?.name?.split(" ")[0];
+
   return (
     <div>
-      <h1>Guest area</h1>
-      <p>This is the guest area page of our application.</p>
+      <h1 className="text-3xl font-semibold mb-6 text-accent-500">
+        Welcome, {firstName}
+      </h1>
     </div>
   );
 }
